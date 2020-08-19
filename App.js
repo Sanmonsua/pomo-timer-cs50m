8d6fe4eee3f8ef74dc67efeba7d252ba1d339e32
Sanmonsua <sanmonsua@gmail.com>
2020-08-19 15:31:17 -0500

    Add : reset, start, stop buttons and funtions

--- App.js
+++ App.js
@@ -1,87 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button, Vibration } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button, Vibration } from "react-native";
 
 export default class App extends React.Component {
   constructor(props) {
-    super (props)
+    super(props);
     this.state = {
-      time : 1 * 60,
-      minutes : 1,
-      seconds : 0,
-      isPaused : false,
-    }
-    
+      starting_minutes : 25,
+      time: 25 * 60,
+      minutes: 25,
+      seconds: 0,
+      isPaused: true,
+    };
   }
-  
-  componentDidMount(){
-    this.timer = setInterval(this.updateTime, 1000)
+
+  componentDidMount() {
+    this.timer = setInterval(this.updateTime, 1000);
   }
 
   updateTime = () => {
-    if (this.state.time >= 0){
-      if (!this.state.isPaused ){
-        this.setState(prevState => ({
-          minutes : Math.floor(prevState.time/60),
-          seconds : prevState.time % 60,
-          time : prevState.time - 1,
-        }))
+    if (this.state.time >= 0) {
+      if (!this.state.isPaused) {
+        this.setState((prevState) => ({
+          minutes: Math.floor(prevState.time / 60),
+          seconds: prevState.time % 60,
+          time: prevState.time - 1,
+        }));
       }
+    } else {
+      Vibration.vibrate([500, 500, 500]);
+      console.log("vibrating");
     }
-    else {
-      Vibration.vibrate([500, 500, 500])
-      console.log('vibrating')
-    }
-    
-  }
+  };
 
-  pause = () =>{
+  pausePlay = () => {
+    this.setState(prevState =>({
+      isPaused: !prevState.isPaused,
+    }));
+  };
+
+  reset = () => {
     this.setState({
+      time : this.state.starting_minutes*60,
+      minutes : this.state.starting_minutes,
+      seconds : 0,
       isPaused : true,
-    })
-  }
+    });
+  };
 
-  play = () =>{
-    this.setState({
-      isPaused : false,
-    })
-  }
-  
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.time}>{this.state.minutes} : {this.state.seconds < 10 ? '0' + this.state.seconds : this.state.seconds}</Text>
+        <Text style={styles.time}>
+          {this.state.minutes} : 
+          {this.state.seconds < 10
+            ? " 0" + this.state.seconds
+            : this.state.seconds}
+        </Text>
         <View style={styles.controllers}>
-          <View style={styles.buttons}><Button title='Pause' onPress={this.pause}/></View>
-          <View style={styles.buttons}><Button title='Play' onPress={this.play}/></View>
-          
+          <View style={styles.buttons}>
+            <Button color="#FF003E" title={this.state.isPaused == true ? "Start" : "Pause"} onPress={this.pausePlay} />
+          </View>
+          <View style={styles.buttons}>
+            <Button color="#FF003E" title="Reset" onPress={this.reset} />
+          </View>
         </View>
-        
+
         <StatusBar style="auto" />
       </View>
-    )
+    );
   }
-  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  time : {
-    fontSize: 60,
-  },
-  controllers : {
-    flexDirection: 'row',
-    justifyContent : "space-between",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
-  buttons : {
-    padding : 10,
-    height : 70,
-    width : '40%',
-  }
+  time: {
+    fontSize: 60,
+  },
+  controllers: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  buttons: {
+    padding: 10,
+    height: 70,
+    width: "40%",
+  },
 });
