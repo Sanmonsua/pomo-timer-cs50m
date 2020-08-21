3eba275ec71bc34fec347438e288a92252b815e9
Sanmonsua <sanmonsua@gmail.com>
2020-08-21 17:39:40 -0500

    Add : optimization and remove redundancies

--- App.js
+++ App.js
@@ -1,104 +1,135 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Button, Vibration, TextInput } from "react-native";
 
-let work = {
-  starting_minutes : 25,
-  time: 25 * 60,
-  minutes: 25,
-  seconds: 0,
-  isPaused: true,
-  name : "Work",
-}
-
-let rest = {
-  starting_minutes : 5,
-  time: 5 * 60,
-  minutes: 5,
-  seconds: 0,
-  isPaused: true,
-  name : "Rest",
-}
-
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = work;
+  state = {
+    work : {
+      starting_minutes : 25,
+      name : "Work",
+      active: true,
+    },
+    rest : {
+      starting_minutes : 5,
+      name : "Rest",
+      active: false,
+    },
+    active : {}
   }
 
-  componentDidMount() {
+  componentDidMount = () => {
+    this.changeToActive()
     this.timer = setInterval(this.updateTime, 1000);
   }
 
+  changeToActive = () => {
+    if (this.state.work.active){
+      this.setState(prevState => ({
+        active : {
+          starting_minutes : prevState.work.starting_minutes,
+          name: prevState.work.name, 
+          time: prevState.work.starting_minutes * 60,
+          minutes: prevState.work.starting_minutes,
+          seconds: 0,
+          isPaused: true,
+        }
+      }))
+    } else {
+      this.setState(prevState => ({
+        active : {
+          starting_minutes : prevState.rest.starting_minutes,
+          name: prevState.rest.name, 
+          time: prevState.rest.starting_minutes * 60,
+          minutes: prevState.rest.starting_minutes,
+          seconds: 0,
+          isPaused: true,
+        }
+      }))
+    }
+  }
+
   updateTime = () => {
-    if (this.state.time >= 0) {
-      if (!this.state.isPaused) {
+    if (this.state.active.time >= 0) {
+      if (!this.state.active.isPaused) {
         this.setState((prevState) => ({
-          minutes : Math.floor(prevState.time / 60),
-          seconds: prevState.time % 60,
-          time: prevState.time - 1,
+          active : {
+            minutes : Math.floor(prevState.active.time / 60),
+            seconds: prevState.active.time % 60,
+            time: prevState.active.time - 1,
+            starting_minutes : prevState.active.starting_minutes,
+            isPaused: prevState.active.isPaused,
+            name : prevState.active.name,
+          }
         }));
       }
     } else {
       Vibration.vibrate([500, 500, 500])
       console.log("vibrating")
-      if (this.state.name === work.name){
-        this.setState(rest)
-        console.log(rest)
-      } else {
-        this.setState(work)
-      }
+      this.setState(prevState => ({
+        work : {
+          name : prevState.work.name,
+          starting_minutes : prevState.work.starting_minutes,
+          active : !prevState.work.active,
+        },
+        rest : {
+          name : prevState.rest.name,
+          starting_minutes : prevState.rest.starting_minutes,
+          active : !prevState.rest.active,
+        }
+      }))
+      this.changeToActive()
     }
   };
 
   pausePlay = () => {
     this.setState(prevState =>({
-      isPaused: !prevState.isPaused,
+      active : {
+        starting_minutes : prevState.active.starting_minutes,
+        isPaused: !prevState.active.isPaused,
+        time : prevState.active.time,
+        minutes : prevState.active.minutes,
+        seconds : prevState.active.seconds,
+        name : prevState.active.name,
+      }
     }));
   };
 
   reset = () => {
-    this.setState({
-      time : this.state.starting_minutes*60,
-      minutes : this.state.starting_minutes,
-      seconds : 0,
-      isPaused : true,
-    });
+    this.setState(prevState => ({
+      active : {
+        starting_minutes : prevState.active.starting_minutes,
+        time : prevState.active.starting_minutes*60,
+        minutes : prevState.active.starting_minutes,
+        seconds : 0,
+        isPaused : true,
+        name : prevState.active.name,
+      }
+    }));
   };
 
-  onChangeTime = (timer, minutes) => {
-    let timer_obj = {}
-    if (minutes.length > 0){
-      timer_obj = { 
-        starting_minutes : parseInt(minutes),
-        time: parseInt(minutes) * 60,
-        minutes: parseInt(minutes),
-        seconds: 0,
-        isPaused: true,
-        name : timer,
-      }
-    } else {
-      timer_obj = { 
-        starting_minutes : 0,
-        time: 0 * 60,
-        minutes: 0,
-        seconds: 0,
-        isPaused: true,
-        name : timer,
-      }
+  onChangeWorkTime = (minutes) => {
+    if (+minutes >= 0){
+      this.setState(prevState => ({
+        work : { 
+          starting_minutes : +minutes,
+          name : prevState.work.name,
+          active : prevState.work.active,
+        }
+      }))
     }
-    
-    if (timer_obj.name === work.name){
-      work = timer_obj 
-    } else {
-      rest = timer_obj
-    }
+    this.changeToActive()
+  }
 
-    if (timer_obj.name === this.state.name){
-      this.setState(timer_obj)
+  onChangeRestTime = (minutes) => {
+    if (+minutes >= 0){
+      this.setState(prevState => ({
+        rest : { 
+          starting_minutes : +minutes,
+          name : prevState.rest.name,
+          active : prevState.rest.active,
+        }
+      }))
     }
-
-    console.log(this.state)
+    this.changeToActive()
   }
 
 
@@ -107,34 +138,53 @@ export default class App extends React.Component {
       <View style={{flex:1}}>
        
         <View style={styles.container}>
-          <Text style={styles.title}>{this.state.name} time</Text>
+          <Text style={styles.title}>
+            {this.state.active.name} time
+          </Text>
           <Text style={styles.time}>
-            {this.state.minutes} : 
-            {this.state.seconds < 10
-              ? " 0" + this.state.seconds
-              : this.state.seconds}
+            {this.state.active.minutes} : 
+            {this.state.active.seconds < 10
+              ? " 0" + this.state.active.seconds
+              : this.state.active.seconds}
           </Text>
           <View style={styles.controllers}>
             <View style={styles.buttons}>
-              <Button color="#FF003E" title={this.state.isPaused == true ? "Start" : "Pause"} onPress={this.pausePlay} />
+              <Button 
+                color="#FF003E"
+                title={this.state.active.isPaused == true ? "Start" : "Pause"} 
+                onPress={this.pausePlay} 
+              />
             </View>
             <View style={styles.buttons}>
-              <Button color="#FF003E" title="Reset" onPress={this.reset} />
+              <Button 
+                color="#FF003E" 
+                title="Reset" 
+                onPress={this.reset} 
+              />
             </View>
           </View>
           <View style={styles.inputs}>
             <View style={styles.timerInputHolder}>
-              <Text style={{fontSize:20, color:'#fff'}}>Work</Text>
-              <TextInput onChangeText={text => this.onChangeTime("Work", text)} style={{fontSize:35, color:'#fff'}} keyboardType="numeric" defaultValue="25"/>
+              <Text style={{fontSize:20, color:'#fff'}}>
+                {this.state.work.name}
+              </Text>
+              <TextInput 
+                onChangeText={text => this.onChangeWorkTime(text)} 
+                style={{fontSize:35, color:'#fff'}} 
+                keyboardType="numeric" 
+                value={"" + this.state.work.starting_minutes}/>
             </View>
             <View style={styles.timerInputHolder}>
-              <Text style={{fontSize:20, color:'#fff'}}>Rest</Text>
-              <TextInput onChangeText={text => this.onChangeTime("Rest", text)} style={{fontSize:35, color:'#fff'}} keyboardType="numeric" defaultValue="5"/>
+              <Text style={{fontSize:20, color:'#fff'}}>
+              {this.state.rest.name}
+              </Text>
+              <TextInput 
+                onChangeText={text => this.onChangeRestTime(text)}
+                style={{fontSize:35, color:'#fff'}} 
+                keyboardType="numeric" 
+                value={"" + this.state.rest.starting_minutes}/>
             </View>
-            
           </View>
-
-          <StatusBar style="auto" />
         </View>
       </View>
       
